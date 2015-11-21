15583c058cab3b0d3ee51f6ad7318566e0f53435
Leonid Makarov <leonid.makarov@ffwagency.com>
2015-11-20 20:31:34 -0500

    Refactor 2.2 image based on 2.4 code base

--- apache2.2/startup.sh
+++ apache2.2/startup.sh
@@ -4,18 +4,13 @@ set -e
 echo "Configuring Apache2 environment variables..."
 
 # Set ServerName of created container
-if [ -z APACHE_SERVERNAME ]; then
-	export APACHE_SERVERNAME=$(cat /etc/hostname)
+if [ -z $APACHE_SERVERNAME ]; then
+	export APACHE_SERVERNAME="$(cat /etc/hostname)"
 fi
 
 # Set APACHE_FCGI_HOST_PORT to PHP FPM daemon in the CLI container
 if [ -z $APACHE_FCGI_HOST_PORT ]; then
-	export APACHE_FCGI_HOST_PORT=${CLI_1_PORT_9000_TCP_ADDR}:9000
-fi
-
-# docroot path override
-if [ ! -z $DOCROOT ]; then
-	export APACHE_DOCUMENTROOT=$DOCROOT
+	export APACHE_FCGI_HOST_PORT="${CLI_1_PORT_9000_TCP_ADDR}:9000"
 fi
 
 # Execute passed CMD arguments
