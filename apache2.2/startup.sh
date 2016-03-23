ee0d18330e545bb89f02f33e4307ca7e94a03f17
Leonid Makarov <leonid.makarov@ffwagency.com>
2016-03-22 21:04:02 -0400

    Added support for Basic HTTP Authentication

--- apache2.2/startup.sh
+++ apache2.2/startup.sh
@@ -13,5 +13,15 @@ if [ -z $APACHE_FCGI_HOST_PORT ]; then
 	export APACHE_FCGI_HOST_PORT="${CLI_1_PORT_9000_TCP_ADDR}:9000"
 fi
 
+# Basic HTTP Authentication
+export APACHE_AUTH_OFF=''
+if [[ $APACHE_BASIC_AUTH_USER != '' && $APACHE_BASIC_AUTH_PASS != '' ]]; then
+	echo "Enabling Basic HTTP Authentication [${APACHE_BASIC_AUTH_USER}:${APACHE_BASIC_AUTH_PASS}]"
+	htpasswd -cb /opt/htpasswd $APACHE_BASIC_AUTH_USER $APACHE_BASIC_AUTH_PASS
+else
+	# A simple way to disable authentication
+	export APACHE_AUTH_OFF='Satisfy Any'
+fi
+
 # Execute passed CMD arguments
 exec "$@"
