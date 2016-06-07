9b3462f11c2c37e1067b9afd4a77cd3ef055bae8
Leonid Makarov <leonid.makarov@ffwagency.com>
2016-06-07 11:47:29 -0400

    Improved handling of the cli service address/name
    If CLI_1_PORT_9000_TCP_ADDR is undefined (version 2 docker-compose.yml) - default to "cli"

--- apache2.4/startup.sh
+++ apache2.4/startup.sh
@@ -4,14 +4,11 @@ set -e
 echo "Configuring Apache2 environment variables..."
 
 # Set ServerName of created container
-if [ -z $APACHE_SERVERNAME ]; then
-	export APACHE_SERVERNAME="$(cat /etc/hostname)"
-fi
+export APACHE_SERVERNAME="${APACHE_SERVERNAME:-$(cat /etc/hostname)}"
 
 # Set APACHE_FCGI_HOST_PORT to PHP FPM daemon in the CLI container
-if [ -z $APACHE_FCGI_HOST_PORT ]; then
-	export APACHE_FCGI_HOST_PORT="${CLI_1_PORT_9000_TCP_ADDR}:9000"
-fi
+# If CLI_1_PORT_9000_TCP_ADDR is undefined (version 2 docker-compose.yml) - default to "cli"
+export APACHE_FCGI_HOST_PORT="${APACHE_FCGI_HOST_PORT:-${CLI_1_PORT_9000_TCP_ADDR:-cli}:9000}"
 
 # Basic HTTP Authentication
 export APACHE_AUTH_OFF=''
