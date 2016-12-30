0adef3ddf4298bc40be894c543b3ee56b230fe8c
Leonid Makarov <leonid.makarov@ffwagency.com>
2016-12-29 17:27:02 -0800

    Remove legacy linked containers ENV variable support

--- apache2.4/startup.sh
+++ apache2.4/startup.sh
@@ -8,7 +8,7 @@ export APACHE_SERVERNAME="${APACHE_SERVERNAME:-$(cat /etc/hostname)}"
 
 # Set APACHE_FCGI_HOST_PORT to PHP FPM daemon in the CLI container
 # If CLI_1_PORT_9000_TCP_ADDR is undefined (version 2 docker-compose.yml) - default to "cli"
-export APACHE_FCGI_HOST_PORT="${APACHE_FCGI_HOST_PORT:-${CLI_1_PORT_9000_TCP_ADDR:-cli}:9000}"
+export APACHE_FCGI_HOST_PORT="${APACHE_FCGI_HOST_PORT:-cli:9000}"
 
 # Basic HTTP Authentication
 export APACHE_AUTH_OFF=''
